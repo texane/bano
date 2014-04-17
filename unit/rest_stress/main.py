e84c25ab56d7189a0c34b1395ca944fe44566963
texane <texane@gmail.com>
2014-04-17 08:19:08 -0500

    [ update ] unit/rest_stress

--- unit/rest_stress/main.py
+++ unit/rest_stress/main.py
@@ -12,20 +12,21 @@ kv['op'] = 'set'
 kv['is_ack'] = 1
 kv['fmt'] = 'plain'
 
-conn = httplib.HTTPConnection("dedibox:1180")
+# conn = httplib.HTTPConnection("freebox:1180")
+conn = httplib.HTTPConnection("192.168.0.11:80")
 
 npass = 0
 while True:
  if kv['val'] == 1: kv['val'] = 0
  else: kv['val'] = 1
  params = urllib.urlencode(kv)
- print(str(params))
- conn.request("GET", "/", params)
+ conn.request("GET", "/?", params)
  resp = conn.getresponse()
  compl_data = eval(resp.read())
  compl_err = compl_data[0]
  compl_val = compl_data[1]
- if compl_err != 0: print("err=" + str(compl_err) + " at " + str(npass))
+ if compl_err: print("err=" + str(compl_err) + " at " + str(npass))
+ if npass % 10 == 0: print("ok at npass = " + str(npass))
  npass = npass + 1
 
 conn.close()
