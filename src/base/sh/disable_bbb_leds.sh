dc5d6bd30064a0b326c9f0630c9318f87a64ff76
texane <texane@gmail.com>
2014-05-14 18:36:08 -0500

    [ add ] sh/disable_bbb_leds.sh

---
+++ src/base/sh/disable_bbb_leds.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+echo none > /sys/class/leds/beaglebone\:green\:usr0/trigger
+echo none > /sys/class/leds/beaglebone\:green\:usr1/trigger
+echo none > /sys/class/leds/beaglebone\:green\:usr2/trigger
+echo none > /sys/class/leds/beaglebone\:green\:usr3/trigger
