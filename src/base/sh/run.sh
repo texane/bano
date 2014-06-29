f19df67adfc9ce002fe735f35a02edaca6504c8a
texane <texane@gmail.com>
2014-06-28 21:56:40 -0500

    [ fix ] base/sh/run.sh

--- src/base/sh/run.sh
+++ src/base/sh/run.sh
@@ -5,9 +5,7 @@ if [ -z "$BANO_SMS_KEY" ]; then
  exit ;
 fi
 
-if [ ! -x ./sh/disable_bbb_leds.sh ]; then
- chmod 755 ./sh/disable_bbb_leds.sh ;
-fi
-
+chmod 755 ./sh/disable_bbb_leds.sh ;
 ./sh/disable_bbb_leds.sh ;
-./a.out -op_name listen > /dev/null 2>&1 & ;
+
+./a.out -op_name listen > /dev/null 2>&1 &
